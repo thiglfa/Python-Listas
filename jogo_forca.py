e4d4f13c475cbc284f36e964e2d2bce712775218
thiglfa <thiagllima26@gmail.com>
2024-04-25 09:15:30 -0300

    Listas de Python e jogo da forca

---
+++ jogo_forca.py
@@ -0,0 +1,32 @@
+sorteada = "frango assado"
+erros = 0
+chutadas = " "
+
+segredo = ""
+for c in sorteada:
+    if c in chutadas:
+        segredo = segredo + c + " "
+    else:
+        segredo = segredo + "_ "
+
+while erros < 6 and "_" in segredo:
+    print(f"{segredo}\nerros: {erros}")
+    letra = input("Informe a letra: ").lower()
+    chutadas = chutadas + letra
+
+    segredo = ""
+    for c in sorteada:
+        if c in chutadas:
+            segredo = segredo + c + " "
+        else:
+            segredo = segredo + "_ "
+    if not letra in sorteada:
+        erros = erros + 1
+if erros == 6:
+    print(f"Você errou {erros} vezes e a palavra era {sorteada}")
+else:
+    print(f"Parabéns, você acertou {sorteada}")
+
+#pegue a letra digitada e gere o novo segredo
+#suponha que a letra informada seja o a
+#segredo -> _ _ a _ _    a _ _ a _ _
